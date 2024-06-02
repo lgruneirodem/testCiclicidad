b305b880b62319cbb0aa37ca87d990b1daab0c1a
Ines <nes.maria.montero@gmail.com>
2024-06-02 14:59:04 +0200

    sustitucion de archivo

--- TestCiclicidad.java
+++ TestCiclicidad.java
@@ -1,47 +1,21 @@
 
-import java.util.ArrayList;
-import Practica4.Grafo;
+public class TestCiclicidad {
 
-
-/**
- * 
- * @author1 Apellido1 Apellido2, Nombre
- * @expediente1 12345678
- * @author2 Apellido3 Apellido4, Nombre
- * @expediente2 87654321
- * @date 2024-05-10
- *
- */
-
-
-
-public class TestCiclicidad <Clave, InforVertice, Coste> {
-	
-	public boolean testCiclos(Grafo<Clave, InforVertice, Coste> grafo) {
-		
-		for(int i=0 ;i < grafo.numVertices();i++) {
-			if(tieneCiclo(grafo,grafo.listaVertices().consultar(i), visitados,actual)){
-				return false;
+	public <Clave, InfoVertice, Coste> boolean testCiclos (Grafo<Clave, InfoVertice, Coste> grafo) {
+		if(!grafo.esVacio()) {
+			boolean ciclos = false;
+			Lista <Par<Clave,Clave>> aristas=grafo.listaAristas();
+			int contador=1;
+			while(contador<=aristas.longitud() && ciclos==false) {
+				if(aristas.consultar(contador).getAtributo()==aristas.consultar(contador).getValor()) {
+					ciclos=true;
+				}
 			}
+			return ciclos;
 		}
-		
-		return true;
-	}
-	
-	private boolean tieneCiclo(Grafo<Clave, InfoVertice, Coste> grafo, Clave v, ArrayList<Clave> visitados, ArrayList<Clave> actual) {
-		if (actual.contains(v)) {
-			return true;
-		}
-		if (visitados.contains(v)) {
-			return false;
-		}
-
-		actual.remove(v);
 		return false;
+		
 	}
-	
-
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
